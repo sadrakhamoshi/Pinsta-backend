7f878418713dc21dfb8e0254d4594aa16d811a29
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-09-03 18:15:54 +0430

    add upload to attribute to avatar models.py

--- pinsta/account/models.py
+++ pinsta/account/models.py
@@ -1,9 +1,18 @@
+import os
+
 from django.db import models
+from uuid import uuid4
 from django.contrib.auth.models import AbstractUser
 
 
+def image_path_generator(instance, filename):
+    type = filename.split('.')[-1]
+    new_filename = '{}.{}'.format(uuid4().hex, type)
+    return os.path.join('pictures/', new_filename)
+
+
 class User(AbstractUser):
-    avatar = models.ImageField(blank=True, null=True)
+    avatar = models.ImageField(upload_to=image_path_generator, blank=True, null=True)
     REQUIRED_FIELDS = ['email', ]
 
     class Meta:
