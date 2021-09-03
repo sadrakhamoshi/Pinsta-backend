4fba774ea544e763f08be9bb4d82ea26fb1487b2
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-09-04 01:00:20 +0430

    update urls for pages

---
+++ pinsta/pageAD/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .viewsets import *
+
+urlpatterns = [
+    path('explore', page_ad_get),
+    path('', page_ad_create),
+    path('my_pages', page_ad_mine),
+    path('<int:pk>', page_ad_detail),
+]
