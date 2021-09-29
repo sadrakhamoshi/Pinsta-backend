b91f1e73d8f51fda8e52ca41f0b81eac3bbcc69a
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-09-29 12:07:59 +0330

    add pageAdReqeustViewSet

--- pinsta/pageAD/views.py
+++ pinsta/pageAD/views.py
@@ -1,8 +1,13 @@
+from datetime import datetime
+from datetime import timedelta
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
 from .models import *
 from .serizlizers import *
+from django.http import JsonResponse
+
+from rest_framework.permissions import IsAdminUser
 from django.db.models import Q
 from django.contrib.auth.models import AnonymousUser
 
@@ -41,14 +46,32 @@ class PageADViewSet(ModelViewSet):
 
 class FavoritePagesViewSet(ModelViewSet):
     queryset = FavoritePage.objects.all()
-
     pass
 
 
 class PageAdRequestViewSet(ModelViewSet):
     queryset = PageAdRequest.objects.all()
+    serializer_class = RequestPageAdListSerializer
+
+    # TODO add permission class for admin and normal users
+    # permission_classes = [IsAdminUser, ]
 
     def get_serializer_class(self):
-        if self.action == 'create':
-            return PageADCreateSerializer
-        return PageADListSerializer
+        if self.action == "create":
+            return RequestPageAdSerializer
+        return self.serializer_class
+
+    def accept(self, request, *args, **kwargs):
+        id = kwargs.get('pk')
+        request_page_ad = self.queryset.get(id=id)
+        pageAd = request_page_ad.page_ad
+        if request_page_ad.membership == 'w':
+            days = 7
+        elif request_page_ad.membership == 'm':
+            days = 30
+        else:
+            days = 365
+        pageAd.deadline = datetime.now() + timedelta(days=days)
+        pageAd.save()
+        request_page_ad.delete()
+        return JsonResponse({'msg': 'successfully accepted'}, safe=False, status=status.HTTP_200_OK)
