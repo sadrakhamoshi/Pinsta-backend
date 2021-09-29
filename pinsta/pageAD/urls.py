1488dcde7d77d1b31010c402efa212d71c3c88bd
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-09-29 12:03:02 +0330

    add url for pageAd request

--- pinsta/pageAD/urls.py
+++ pinsta/pageAD/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('my_pages', page_ad_mine),
     path('<int:pk>', page_ad_detail),
     path('search', page_ad_search),
+    path('request', request_page_ad_create),
+    path('requests', request_page_ad_list),
+    path('request/reject/<int:pk>', request_page_ad_reject),
+    path('request/accept/<int:pk>', request_page_ad_accept),
 ]
--- pinsta/pageAD/viewsets.py
+++ pinsta/pageAD/viewsets.py
@@ -1,4 +1,4 @@
-from .views import PageADViewSet
+from .views import PageADViewSet, PageAdRequestViewSet
 
 page_ad_get = PageADViewSet.as_view({
     'get': 'list',
@@ -19,4 +19,20 @@ page_ad_mine = PageADViewSet.as_view({
 
 page_ad_search = PageADViewSet.as_view({
     'get': 'search',
+})
+
+request_page_ad_create = PageAdRequestViewSet.as_view({
+    'post': 'create'
+})
+
+request_page_ad_list = PageAdRequestViewSet.as_view({
+    'get': ' list'
+})
+
+request_page_ad_reject = PageAdRequestViewSet.as_view({
+    'delete': 'destroy',
+})
+
+request_page_ad_accept = PageAdRequestViewSet.as_view({
+    'delete': 'accept',
 })
