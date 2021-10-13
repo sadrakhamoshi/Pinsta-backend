0f1a4dc4e8366d14b922b584a061601b77c7126d
sadrakhamoshi <s.a.d.kh.121@gmail.com>
2021-10-14 01:24:07 +0330

    change serializer for create page_ad

--- pinsta/pageAD/views.py
+++ pinsta/pageAD/views.py
@@ -25,11 +25,6 @@ class PageADViewSet(ModelViewSet):
             return self.queryset.exclude(owner=self.request.user)
         return super(PageADViewSet, self).get_queryset()
 
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return PageADCreateSerializer
-        return self.serializer_class
-
     def get_my_page(self, request, *args, **kwargs):
         datas = self.queryset.filter(owner=request.user)
         return Response({'pages': self.serializer_class(datas, many=True).data}, status=status.HTTP_200_OK)
